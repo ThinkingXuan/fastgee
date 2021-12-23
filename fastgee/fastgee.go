3aa949e1e922a1002c0cc10b005b1618e0be24df
youxuan <1454739828@qq.com>
2021-12-23 16:34:26 +0800

    add readme

--- fastgee/fastgee.go
+++ fastgee/fastgee.go
@@ -1,6 +1,7 @@
 package fastgee
 
 import (
+	"log"
 	"net/http"
 )
 
@@ -10,28 +11,41 @@ type HandlerFunc func(*Context)
 // Engine implement the interface of server http
 type Engine struct {
 	//router map[string]HandlerFunc
+	*RouterGroup
 	router *router
+	groups []*RouterGroup // store all groups
 }
+
+/*
+RouterGroup 持有Engine指针，Engine又继承RouterGroup。
+原理： Engine 需要用有管理路由的功能， RouterGroup也要有管理路由的功能，
+      防止功能重叠，所以让他们相互持有。Engine拥有RouterGroup所有功能。
+	  RouterGroup持有持有Engine指针也可以进行路由控制。
+
+*/
 
 // New is the constructor of fastgee Engine
 func New() *Engine {
-	return &Engine{router: newRouter()}
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
 }
 
-// addRoute
-func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
-	engine.router.addRoute(method, pattern, handler)
-}
-
-// GET defines the method to add GET request
-func (engine *Engine) GET(pattern string, handler HandlerFunc) {
-	engine.addRoute("GET", pattern, handler)
-}
-
-// POST defines the method to add POST request
-func (engine *Engine) POST(pattern string, handler HandlerFunc) {
-	engine.addRoute("POST", pattern, handler)
-}
+//// addRoute
+//func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
+//	engine.router.addRoute(method, pattern, handler)
+//}
+//
+//// GET defines the method to add GET request
+//func (engine *Engine) GET(pattern string, handler HandlerFunc) {
+//	engine.addRoute("GET", pattern, handler)
+//}
+//
+//// POST defines the method to add POST request
+//func (engine *Engine) POST(pattern string, handler HandlerFunc) {
+//	engine.addRoute("POST", pattern, handler)
+//}
 
 // Run defines the method to start a http server
 func (engine *Engine) Run(addr string) (err error) {
@@ -42,4 +56,42 @@ func (engine *Engine) Run(addr string) (err error) {
 func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	c := newContext(w, req)
 	engine.router.handle(c)
+}
+
+type RouterGroup struct {
+	prefix string
+	// support middleware
+	middleware []HandlerFunc
+	parent     *RouterGroup
+	// all groups share a engin instance
+	engine *Engine
+}
+
+// Group is defined to create a new RouterGroup
+// remember all groups share the same Engine instance
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := group.engine
+	newGroup := &RouterGroup{
+		prefix: group.prefix + prefix,
+		parent: group,
+		engine: engine,
+	}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
+}
+
+func (group *RouterGroup) addRoute(method string, comp string, handler HandlerFunc) {
+	pattern := group.prefix + comp
+	log.Printf("Route %4s - %s", method, pattern)
+	group.engine.router.addRoute(method, pattern, handler)
+}
+
+// GET defines the method to add GET request
+func (group *RouterGroup) GET(pattern string, handler HandlerFunc) {
+	group.addRoute("GET", pattern, handler)
+}
+
+// POST defines the method to add POST request
+func (group *RouterGroup) POST(pattern string, handler HandlerFunc) {
+	group.addRoute("POST", pattern, handler)
 }
--- main.go
+++ main.go
@@ -8,21 +8,33 @@ import (
 
 func main() {
 	r := fastgee.New()
-	r.GET("/", func(c *fastgee.Context) {
-		c.HTML(http.StatusOK, "<h1>Hello FastGee</h1>")
+	r.GET("/index", func(c *fastgee.Context) {
+		c.HTML(http.StatusOK, "<h1>Index Page</h1>")
 	})
-	r.GET("/hello", func(c *fastgee.Context) {
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
-	})
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/", func(c *fastgee.Context) {
+			c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+		})
 
-	r.GET("/hello/:name", func(c *fastgee.Context) {
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
-	})
-
-	r.GET("/assets/*filepath", func(c *fastgee.Context) {
-		c.JSON(http.StatusOK, fastgee.H{"filepath": c.Param("filepath")})
-	})
-
+		v1.GET("/hello", func(c *fastgee.Context) {
+			// expect /hello?name=geektutu
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+		})
+	}
+	v2 := r.Group("/v2")
+	{
+		v2.GET("/hello/:name", func(c *fastgee.Context) {
+			// expect /hello/geektutu
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+		v2.POST("/login", func(c *fastgee.Context) {
+			c.JSON(http.StatusOK, fastgee.H{
+				"username": c.PostForm("username"),
+				"password": c.PostForm("password"),
+			})
+		})
+	}
 	if err := r.Run(":8080"); err != nil {
 		log.Fatal(err)
 	}
